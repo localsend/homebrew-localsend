699d4da5b36832bd498a14e4042a3c5d73493545
GitHub Action <action@github.com>
2023-05-06 00:43:21 +0000

    Update LocalSend cask to v1.9.1

--- Casks/localsend.rb
+++ Casks/localsend.rb
@@ -1,6 +1,6 @@
 cask "localsend" do
-  version "1.9.0"
-  sha256 "21ba23720e53a36f3c73ebfa20a30e8ee2f3f7bd28b2228953bfcad159c8b3c9"
+  version "1.9.1"
+  sha256 "197c854ac4cadd2f278708b10e0653404c11b7748e72c9db011cdbfc185393cc"
 
   url "https://github.com/localsend/localsend/releases/download/v#{version}/LocalSend-#{version}.dmg"
   name "LocalSend"
