43083ed700586485cdc706138810db718ec0713e
GitHub Action <action@github.com>
2024-02-26 16:10:31 +0000

    Update LocalSend cask to v1.14.0

--- Casks/localsend.rb
+++ Casks/localsend.rb
@@ -1,6 +1,6 @@
 cask "localsend" do
-  version "1.13.1"
-  sha256 "602cba3659843ec385b48676ee63980e730f775b63dd83b66f034374cdcafee6"
+  version "1.14.0"
+  sha256 "2fb578f10a0e034723c78e67fbd5dabffcf30b30ac641dd3062a74586bac3178"
 
   url "https://github.com/localsend/localsend/releases/download/v#{version}/LocalSend-#{version}.dmg"
   name "LocalSend"
