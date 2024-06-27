47ee730c6479ad2cea4bbc05975812694d70e003
chris <chunghao777@gmail.com>
2024-06-27 11:01:29 +0800

    [fixed] Bug: Incorrect depends_on version #3

--- Casks/localsend.rb
+++ Casks/localsend.rb
@@ -7,7 +7,7 @@ cask "localsend" do
   desc "An open source cross-platform alternative to AirDrop"
   homepage "https://localsend.org"
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "LocalSend.app"
 end
